98223e2aacb2fb98fc8f35e428f60f1127d47053
Sultan9463 <s75696643@gmail.com>
2025-06-02 21:38:11 +0100

    Add files via upload

---
+++ remove_duplicates.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    // demande la taille du tableau
+    scanf("%d", &n);
+    int tab[n];
+    //lire les element du tableau
+    for (int i = 0 ; i < n; i++) {
+        scanf("%d", &tab[i]);
+    }
+    printf("Après supression : ");
+    for (int i = 0 ; i < n; i++) {
+        int est_doublon = 0;
+        for (j = 0; j < i; j++) {
+            if (tab[i] == tab[j]) {
+                est_doublon = 1;
+                break;
+            }
+        }
+        if (!est_doublon) {
+            printf("%d", tab[i]);
+        }
+    }
+    printf("\n");
+    return 0;
+}
